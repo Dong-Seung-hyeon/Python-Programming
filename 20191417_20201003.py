b02ba15865035091e5790e4e45e20873c5038bab
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-03 20:25:42 +0900

    Chapter 03 연산자 예제 3-15

--- 20191417_20201003.py
+++ 20191417_20201003.py
@@ -171,3 +171,23 @@ print('a & b = ', a & b, ":", bin(a & b))
 print('a | b = ', a | b, ":", bin(a | b))
 print('a ^ b = ', a ^ b, ":", bin(a ^ b))
 print('~a = ', ~a, ":", bin(~a))
+
+# ex 3-15
+a = int(input('숫자를 입력하세요 : '))
+print()
+print('a = ', a, ":", bin(a))
+print('---- < left shift > ----')
+a = a << 1 # * 2
+print('a << 1 = ', a, ":", bin(a))
+a = a << 2 # * 4
+print('a << 2 = ', a, ":", bin(a))
+a = a << 3 # * 2**3
+print('a << 3 = ', a, ":", bin(a))
+print()
+print('---- < right shift > ----')
+a = a >> 1 # / 2
+print('a >> 1 = ', a, ":", bin(a))
+a = a >> 2 # / 2
+print('a >> 2 = ', a, ":", bin(a))
+a = a >> 3 # / 2**3
+print('a >> 3 = ', a, ":", bin(a))
