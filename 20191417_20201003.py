f66f209264ead39bc20c4148bda3e1667d859d6a
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-03 17:31:36 +0900

    Chapter 03 연산자 예제 3-8

--- 20191417_20201003.py
+++ 20191417_20201003.py
@@ -101,3 +101,12 @@ month2 = birth // 100 % 100
 date = birth % 100
 print(year, month1, date)
 print("생년월일 : {}년 {}월 {}일".format(year, month2, date))
+
+# ex 3-8
+celsius = int(input('섭씨온도를 입력하세요 : '))
+fahrenheit = (9/5) * celsius + 32
+print('섭씨', celsius, '도는 화씨', fahrenheit, '도 입니다.')
+print('=' * 30)
+fahrenheit = float(input('화씨온도를 입력하세요 : '))
+celsius = int((fahrenheit - 32) *(5/9))
+print('화씨', fahrenheit,'도는 섭씨', celsius, '도 입니다.')
