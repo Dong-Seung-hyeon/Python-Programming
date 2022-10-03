718599b7f766300817b8dea040ca501118bf402a
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-03 21:14:21 +0900

    Chapter 03 연산자 예제 3-17

--- 20191417_20201003.py
+++ 20191417_20201003.py
@@ -200,3 +200,27 @@ c = a - b
 d = a + (~b + 1)
 print('a - b = ', c)
 print('a + (~b + 1) = ', d)
+
+# ex 3-17
+a = int(input('첫 번째 수를 입력하세요 : '))
+b = int(input('첫 번째 수를 입력하세요 : '))
+print('-' * 30 )
+print('두 개의 숫자를 교환하기\n')
+print('a={0}, b={1}'.format(a,b))
+print('-' * 30 )
+print('임시변수를 사용하는 방법\n')
+temp = a
+a = b
+b = temp
+print('a={0}, b={1}'.format(a,b))
+print('-' * 30 )
+print('^(xor) 연산자를 이용하는 방법\n')
+print('a = ', a, ":", bin(a))
+print('b = ', b, ":", bin(b), '\n')
+a = a ^ b
+print('a = a ^ b = ', a, ":", bin(a))
+b = a ^ b
+print('b = a ^ b = ', b, ":", bin(b))
+a = a ^ b
+print('a = a ^ b = ', a, ":", bin(a), '\n')
+print('a={0}, b={1}'.format(a,b))
