f47d23b302a54675a928cb5306227d98844db4c7
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-05 16:42:08 +0900

    Chapter 04 반복문 예제 4-14

--- 20191417_20221005.py
+++ 20191417_20221005.py
@@ -244,3 +244,13 @@ for n in range(1, 11):
     sum += n
 print(sum)
 
+# ex 4-14
+sum_even = 0 # 짝수의 합
+sum_odd = 0  # 홀수의 합
+for n in range(1, 101): # 1부터 100까지의 반복
+    if n % 2 == 0:      # n 이 짝수인 경우,
+        sum_even += n
+    else :              # n 이 홀수인 경우,
+        sum_odd += n
+print('짝수의 합 = ' , sum_even)
+print('홀수의 합 = ' , sum_odd)
