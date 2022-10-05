7d1f9d21442c3db672f3fc3be29100c528fc287d
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-05 15:10:12 +0900

    Chapter 04 연산자 예제 4-6

--- 20191417_20221005.py
+++ 20191417_20221005.py
@@ -45,3 +45,12 @@ if age >= 20 :
 else : 
     print('너무 어려서 불합격입니다!')
 
+# ex 4-6
+money = int(input('지갑에 얼마 있습니까?  '))
+if money >= 3000:
+    print('과자를 사먹자~~')
+elif money >= 1000:
+    print('껌을 사먹어야지^^')
+else : 
+    print('공기나 마시자 ㅠ.ㅠ')
+
