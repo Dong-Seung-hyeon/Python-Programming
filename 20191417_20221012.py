bea0f96de29d0b1d4c3c938b88c4967c7ded067d
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-12 15:59:28 +0900

    Chapter 04 반복문 예제 4-27

--- 20191417_20221012.py
+++ 20191417_20221012.py
@@ -68,3 +68,10 @@ for i in range(1, x+1) :
     if not (i % 5 == 0):
         print(i, end = ' ')
 print()
+
+# ex 4-27
+x = int(input('숫자를 입력하세요 : '))
+a = 1
+for i in range(0, x+1):
+    print('{0:6,}'.format(a << i), end=' ')
+print()
