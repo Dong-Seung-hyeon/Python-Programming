1f2b18daad8143e9755bdd740d81c0f301ff877e
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-12 15:06:25 +0900

    Chapter 04 반복문 예제 4-20

--- 20191417_20221012.py
+++ 20191417_20221012.py
@@ -8,3 +8,12 @@ for s in score:
     else:
         print("%d번 학생의 점수는 %d점이며 불합격입니다." % (number, s))
 
+# ex 4-20
+score = [90, 25, 67, 45, 80]
+number = 0
+for s in score:
+    number = number +1
+    if s < 60:
+        continue
+    print("%d번 학생 %d점으로 합격합니다. 축하합니다!" % (number, s))
+
