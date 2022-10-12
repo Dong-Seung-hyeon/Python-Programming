644078a8fae6c166efebd84274c9f8557c415cba
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-12 16:38:03 +0900

    Chapter 04 반복문 예제 4-34

--- 20191417_20221012.py
+++ 20191417_20221012.py
@@ -151,3 +151,19 @@ while True:
         if (num % 10 >= 5) :
             grade += '+'
     print('{0}점의 학점은 "{1}"입니다.\n'.format(num, grade))
+
+# ex 4-34
+while True:
+    num = int(input('정수를 입력하시오(0은 종료)'))
+    if not num:
+        break                # 반복문을 탈출합니다.
+    if num < 0:
+        print('양의 정수로 입력하시오')
+        continue
+    count = 0                # 약수의 개수를 세어줄 변수
+    for i in range(1, num+1):
+        if num % i == 0:     # 나누어지면 약수
+            print('{0:5}'.format(i), end=' ')
+            count += 1
+    print()
+    print('{0}의 약수의 개수 {1}개입니다.\n'.format(num, count))
