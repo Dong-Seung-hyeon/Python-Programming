7951c04b30838d9dd3fc9c5b9cbf9e812127f1ba
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-12 16:32:43 +0900

    Chapter 04 반복문 예제 4-33

--- 20191417_20221012.py
+++ 20191417_20221012.py
@@ -123,3 +123,31 @@ while True :
     print("%d + %d = %d" % (a, b, hap))
 
 print("0을 입력해 반복문을 탈출했습니다")
+
+# ex 4-33
+while True:
+    num = int(input('점수를 입력하세요(음수는 종료) : '))
+    if num < 0:
+        break
+
+    if num > 100:
+        print('올바른 점수가 아닙니다.\n0 ~ 100 사이의 점수를 입력하세요\n')
+        continue    # 반복문은 처음으로 가라!!
+
+    # 학점 구하기
+    if num >= 90:
+        grade = 'A'
+    elif num >= 80:
+        grade = 'B'
+    elif num >= 70:
+        grade = 'C'
+    elif num >= 60:
+        grade = 'D'
+    else :
+        grade = 'F'
+    if num == 100:
+        grade += '+'
+    elif num > 60 :
+        if (num % 10 >= 5) :
+            grade += '+'
+    print('{0}점의 학점은 "{1}"입니다.\n'.format(num, grade))
