84c52ecc8a1f7603acd224a91c25a23774f00998
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-26 15:13:55 +0900

    Chapter 06 함수예제 6-3

--- 20191417_20221026.py
+++ 20191417_20221026.py
@@ -13,3 +13,13 @@ def plus(v1, v2):
 hap = plus(100, 200)
 print(hap)
 
+# ex 6-3
+def plus(v1, v2):
+    """이 함수는 v1과 v2를 더한 뒤 결과를 반환하는 함수입니다."""
+    result = 0
+    result = v1 + v2
+    return result
+
+hap = plus(100, 200)
+print(hap)
+print(plus.__doc__)
