1c3597b1a8fefe6cf27d92409a4aec18a78a4790
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-26 15:42:58 +0900

    Chapter 06 함수예제 6-11

--- 20191417_20221026.py
+++ 20191417_20221026.py
@@ -79,3 +79,11 @@ def student_info(name, phone, id_no='비공개'):
     print('주민번호: ', id_no)
 
 student_info('김철수', '010-1234-5678', '20200101-3145312')
+
+# ex 6-11
+def student_info(name, id_no='비공개', phone):
+    print('이름: ', name)
+    print('휴대폰: ', phone)
+    print('주민번호: ', id_no)
+
+student_info('김철수', '010-1234-5678')
