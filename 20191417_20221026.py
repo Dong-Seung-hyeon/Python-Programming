5d9177088c29d300a27059772e349dfc4716de7f
Dong Seunghyeon <kj94gakt@naver.com>
2022-10-26 15:12:21 +0900

    Chapter 06 함수예제 6-2

--- 20191417_20221026.py
+++ 20191417_20221026.py
@@ -3,3 +3,13 @@ def print_10times_star():
     print('*' * 10 )
 
 print_10times_star()
+
+# ex 6-2
+def plus(v1, v2):
+    result = 0
+    result = v1 + v2
+    return result
+
+hap = plus(100, 200)
+print(hap)
+
