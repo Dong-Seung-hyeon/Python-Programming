fa8609b9d90034d5f3e4983b73b92fbaf63d329b
Dong Seunghyeon <kj94gakt@naver.com>
2022-11-02 15:51:30 +0900

    Chapter 07 클래스와 객체지향 프로그래밍 예제 7-5

--- 20191417-20221102.py
+++ 20191417-20221102.py
@@ -68,3 +68,22 @@ class PythonSchool:
 
 student1 = PythonSchool("Hong kildong", "hong1234@email.com", "010-1234-5678")
 student1.printStud()
+
+# ex 7-5
+class PythonSchool:
+    def __init__(self, name, email, phone):
+
+        self.name = name
+        self.email = email
+        self.phone = phone
+    def printStudent(self):
+        print("이름: ", self.name)
+        print("이메일: ", self.email)
+        print("전화번호: ", self.phone)
+
+student1 = PythonSchool("Hong kildong", "hong1234@email.com", "010-1234-5678")
+student1.printStudent()
+
+del student1
+# student1.printStudent()
+
