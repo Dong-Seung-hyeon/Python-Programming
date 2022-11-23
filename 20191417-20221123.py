9d01e7eebcf9c275ee9482b3dbe97cfd8cbac4fa
Dong Seunghyeon <kj94gakt@naver.com>
2022-11-23 15:23:12 +0900

    Chapter 10 표준 라이브러리 예제 10-15

--- 20191417-20221123.py
+++ 20191417-20221123.py
@@ -7,8 +7,17 @@ print(check.status)
 print(address.urlopen("https://www.naver.com"))
 
 icheck = address.urlopen("http://www.pythonchallenge.com/")
-print(icheck.read())
-print('---------------------------')
+# print(icheck.read())
+# print('---------------------------')
 print(icheck.read().decode('utf-8'))
 
-# ex 10-11
+import urllib.parse
+parse = urllib.parse.urlparse()
+print(parse)
+
+# ex 10-15
+import urllib.robotparser
+rp = urllib.robotparser.RobotFileParser('http://www.sungkyul.ac.kr')
+rp.read()
+print(rp.can_fetch('mycrawler', 'http://www.sungkyul.ac.kr'))
+
