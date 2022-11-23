4bf2f1b5e9f765955ea7b70ebd1a99d29f278191
Dong Seunghyeon <kj94gakt@naver.com>
2022-11-23 15:10:34 +0900

    Chapter 10 표준 라이브러리 예제 10-10

--- 20191417-20221123.py
+++ 20191417-20221123.py
@@ -1,6 +1,14 @@
-#ex 10-10
+# ex 10-10
 import urllib.request
 
 address = urllib.request
 check = address.urlopen("https://www.naver.com")
 print(check.status)
+print(address.urlopen("https://www.naver.com"))
+
+icheck = address.urlopen("http://www.pythonchallenge.com/")
+print(icheck.read())
+print('---------------------------')
+print(icheck.read().decode('utf-8'))
+
+# ex 10-11
