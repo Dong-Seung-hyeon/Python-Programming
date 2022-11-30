e5cc8e4c7162e80faecb9b2bc5f1cbb498747f9e
Dong Seunghyeon <kj94gakt@naver.com>
2022-11-30 14:58:53 +0900

    Chapter 10 표준 라이브러리 예제 10-37, 10-38

---
+++ 20191417-20221130.py
@@ -0,0 +1,13 @@
+# ex 10-37
+import numpy as np
+
+a = np.array([0, 1, 2, 3, 4, ])
+print(a[2])
+print(a[-1])
+
+# ex 10-38
+a1 = np. array(([0, 1, 2], [3, 4, 5]))
+print(a1)
+print(a1[0, 0]) # 첫번째 행의 첫번째 열
+print(a1[0, 1]) # 첫번째 행의 두번째 열
+print(a1[-1, -1]) # 마지막 행의 마지막 열
