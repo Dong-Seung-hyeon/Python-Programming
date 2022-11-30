e395902cc315eec4ecc54e1b3877c9200746eb7e
Dong Seunghyeon <kj94gakt@naver.com>
2022-11-30 14:54:42 +0900

    Chapter 10 표준 라이브러리 예제

--- 20191417-20221123.py
+++ 20191417-20221123.py
@@ -74,3 +74,29 @@ print()
 x = np.array(data)
 print(x)
 print(x * 2)
+print()
+
+a = np.array([1, 2, 3])
+b = np.array([10, 20, 30])
+print(2 * a + b)
+print(a == 2)
+print(b > 10)
+print((a == 2) & (b > 10))
+print()
+
+c = np.array([[0, 1, 2], [3, 4, 5]]) # 2 X 3 array
+print(c)
+print(len(c))                      # 행의 개수
+print(len(c[0]))                   # 열의 개수
+print()
+
+d = np.array([[[1, 2, 3, 4], 
+               [5, 6, 7, 8], 
+               [9, 10, 11, 12]], 
+              [[11, 12, 13, 14], 
+               [15, 16, 17, 18], 
+               [19, 20, 21, 22]]])   # 2 x 3 x 4 array
+print(d)
+print(len(d), len(d[0]), len(d[0][0]))
+print(d.ndim)
+print(d.shape)
